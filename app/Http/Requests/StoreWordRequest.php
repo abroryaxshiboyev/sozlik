d31e2a9cc2c11e79af8bdd2219d5f653584463db
Abror Yaxshiboyev <abroryaxshiboyev2002@gmail.com>
2023-02-13 11:01:53 +0500

    all

--- app/Http/Requests/StoreWordRequest.php
+++ app/Http/Requests/StoreWordRequest.php
@@ -29,7 +29,7 @@ class StoreWordRequest extends FormRequest
             'kiril'=>'required',
             'description_latin'=>'required',
             'description_kiril'=>'required',
-            'audio'=> 'nullable|mimes:ogg,mp3|max:10000'
+            'audio'=> 'nullable'
         ];
     }
 }
