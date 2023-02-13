f34c34ac6d084731b3841d1512f742ae249f662e
Abror Yaxshiboyev <abroryaxshiboyev2002@gmail.com>
2023-02-13 10:58:45 +0500

    all

--- config/app.php
+++ config/app.php
@@ -67,7 +67,8 @@ return [
     |
     */
 
-    'timezone' => env('APP_TIMEZONE', 'UTC'),
+    // 'timezone' => env('APP_TIMEZONE', 'UTC'),
+    'timezone' => 'UTC',
 
     /*
     |--------------------------------------------------------------------------
