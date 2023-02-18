f29272efe112a24220ebb60a688d0c4e2109f835
Abror Yaxshiboyev <abroryaxshiboyev2002@gmail.com>
2023-02-18 17:33:50 +0500

    all

--- routes/api.php
+++ routes/api.php
@@ -43,6 +43,30 @@ Route::post('authenticate',[AuthController::class,'authenticate']);
 
 Route::middleware('auth:sanctum')->group(function(){
 
+// //auth    
+// Route::post('logout',[AuthController::class,'logout']);
+// Route::get('check',[AuthController::class,'check']);
+// //admin create and delete
+// Route::post('admins',[AuthController::class,'createAdmin']);
+// Route::delete('admins/{id}',[AuthController::class,'deleteAdmin']);
+
+// //categories
+// Route::get('categoriesdate',[CategoryController::class,'sortDate']);
+// Route::post('categories',[CategoryController::class,'store']);
+// Route::put('categories/{id}',[CategoryController::class,'update']);
+// Route::delete('categories/{id}',[CategoryController::class,'destroy']);
+
+// //Word
+// Route::get('wordsdate',[WordController::class,'sortDate']);
+// Route::post('words',[WordController::class,'store']);
+// Route::put('words/{id}',[WordController::class,'update']);
+// Route::delete('words/{id}',[WordController::class,'destroy']);
+
+// //Search
+// Route::get('searches',[SearchController::class,'index']);
+// Route::delete('searches/{id}',[SearchController::class,'destroy']);
+
+});
 //auth    
 Route::post('logout',[AuthController::class,'logout']);
 Route::get('check',[AuthController::class,'check']);
@@ -66,5 +90,4 @@ Route::delete('words/{id}',[WordController::class,'destroy']);
 Route::get('searches',[SearchController::class,'index']);
 Route::delete('searches/{id}',[SearchController::class,'destroy']);
 
-});
 
