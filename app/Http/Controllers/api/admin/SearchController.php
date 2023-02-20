f2274c93bedc4f72828f56e2868af7b5271cec4e
Abror Yaxshiboyev <abroryaxshiboyev2002@gmail.com>
2023-02-20 15:44:14 +0500

    all

--- app/Http/Controllers/api/admin/SearchController.php
+++ app/Http/Controllers/api/admin/SearchController.php
@@ -60,11 +60,12 @@ class SearchController extends Controller
                 }else{
                     $letter=$letters[1]['latin'][0];
                 }
-                return $letter;
                 $array=[];
                 foreach ($words as $word){
                     $str=$word['latin'][0];
+                    return $$letter==$str;
                     if($letter==$str)
+                    
                         $array[]=$word;
                     }
                     return $array;
